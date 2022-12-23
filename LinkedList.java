da4fd0b3bb96eb361721aa882611f29a628a9d67
Filipe Sousa <filipess.2011@gmail.com>
2022-12-23 01:07:21 -0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,16 @@
 //You are given the head of a linked list and a key. You have to delete the node that contains this given key
+public class LinkedListExec {
+    public static void main(String[] args) {
+        LinkedList.LinkedListNode head = new LinkedList.LinkedListNode(1);
+        head.next = new LinkedList.LinkedListNode(2);
+        head.next.next = new LinkedList.LinkedListNode(3);
+        head.next.next.next = new LinkedList.LinkedListNode(4);
+
+        int key = 3;
+        head = LinkedList.deleteNode(head, key);
+        System.out.println(head.val+"->"+head.next.val+"->"+head.next.next.val);
+    }
+}
 
 public class LinkedListNode {
     int val;
